6c064dcc69ddd0b5703d452466760ae599e73ff7
Eduardo Quadros <quadros.ea@gmail.com>
2017-09-23 18:27:57 -0300

    Make stack finite in Python.

--- stack.py
+++ stack.py
@@ -1,19 +1,39 @@
 class Stack:
 
-    def __init__(self):
-        self.items = []
-
-    def size(self):
-        return len(self.items)
+    def __init__(self, length):
+        self.items = [None] * length
+        self.index = 0
 
     def empty(self):
-        return self.size() == 0
+        return True if self.index == 0 else False
 
-    def push(self, item):
-        self.items.append(item)
+    def full(self):
+        return True if self.index == len(self.items) else False
+
+    def peek(self):
+
+        if self.empty():
+            raise IndexError("Cannot peek at an empty stack!")
+
+        return self.items[self.index - 1]
 
     def pop(self):
-        if not self.empty():
-            return self.items.pop()
-        else:
-            raise RuntimeError("Cannot pop from an empty stack!")
+
+        if self.empty():
+            raise IndexError("Cannot pop from an empty stack!")
+
+        self.index -= 1
+        item = self.items[self.index]
+        self.items[self.index] = None
+        return item
+
+    def push(self, item):
+
+        if self.full():
+            raise IndexError("Cannot push into a full stack!")
+
+        self.items[self.index] = item
+        self.index += 1
+
+    def size(self):
+        return self.index
