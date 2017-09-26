e8e83ab49b7017cab00c46ff4820c9d0212080bb
Julio Brazil <julio_bbl@hotmail.com>
2017-09-26 15:23:59 -0300

    Corrections on queue

--- Queue/Queue.swift
+++ Queue/Queue.swift
@@ -2,19 +2,13 @@ import Foundation
 
 internal final class Queue<T> {
     private var items: Array<T?>
-    private var index: Int
     
-    internal init(length: Int) {
-        self.items = [T?](repeating: nil, count: length)
-        self.index = 0
+    internal init() {
+        self.items = []
     }
     
     internal func isEmpty() -> Bool {
-        return self.index == 0
-    }
-    
-    internal func isFull() -> Bool {
-        return self.index >= self.items.count
+        return self.items.count == 0
     }
     
     internal func peek() throws -> T? {
@@ -22,7 +16,7 @@ internal final class Queue<T> {
             throw QueueError.emptyQueue(reason: "Cannot peek at an empty queue!")
         }
         
-        return self.items[index - 1]
+        return self.items.first as? T
     }
     
     internal func pop() throws -> T? {
@@ -30,27 +24,20 @@ internal final class Queue<T> {
             throw QueueError.emptyQueue(reason: "Cannot pop at an empty queue!")
         }
         
-        self.index -= 1
-        let item: T? = self.items[index]
-        self.items[index] = nil
+        let item: T? = self.items.first  as? T
+        self.items.remove(at: 0)
         return item
     }
     
     internal func push(item: T?) throws -> Void {
-        guard !self.isFull() else {
-            throw QueueError.fullQueue(reason: "Cannot push into a full stack!")
-        }
-        
-        self.items[index] = item
-        self.index += 1
+        self.items.append(item)
     }
     
     internal func size() -> Int {
-        return self.index
+        return self.items.count
     }
 }
 
 internal enum QueueError: Error {
     case emptyQueue(reason: String)
-    case fullQueue(reason: String)
 }
