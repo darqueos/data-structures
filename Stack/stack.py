8e1c0037087db41b3bf7e0cdd0e8f505f2c2fefa
Eduardo Quadros <quadros.ea@gmail.com>
2017-09-27 20:28:05 -0300

    Add private variables. Add len() implementation.

--- Stack/stack.py
+++ Stack/stack.py
@@ -1,41 +1,41 @@
 # 3.6.2
 
-class Stack:
+class Stack(object):
 
     def __init__(self, length):
-        self.items = [None] * length
-        self.index = 0
+        self._items = [None] * length
+        self._index = 0
 
-    def empty(self):
-        return self.index == 0
+    def __len__(self):
+        return self._index
 
-    def full(self):
-        return self.index == len(self.items)
+    def is_empty(self):
+        return self._index == 0
+
+    def is_full(self):
+        return self._index == len(self._items)
 
     def peek(self):
 
-        if self.empty():
+        if self.is_empty():
             raise IndexError("Cannot peek at an empty stack!")
 
-        return self.items[self.index - 1]
+        return self._items[self._index - 1]
 
     def pop(self):
 
-        if self.empty():
+        if self.is_empty():
             raise IndexError("Cannot pop from an empty stack!")
 
-        self.index -= 1
-        item = self.items[self.index]
-        self.items[self.index] = None
+        self._index -= 1
+        item = self._items[self._index]
+        self._items[self._index] = None
         return item
 
     def push(self, item):
 
-        if self.full():
+        if self.is_full():
             raise IndexError("Cannot push into a full stack!")
 
-        self.items[self.index] = item
-        self.index += 1
-
-    def size(self):
-        return self.index
+        self._items[self._index] = item
+        self._index += 1
