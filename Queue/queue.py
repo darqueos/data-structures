eab8b90bb6b46630d2d2f6c9ffda3adc30671d3f
Eduardo Quadros <quadros.ea@gmail.com>
2017-09-29 20:18:42 -0300

    Add dequeue algorithm for Python queue implementation.

--- Queue/queue.py
+++ Queue/queue.py
@@ -4,25 +4,32 @@ class Queue(object):
 
     def __init__(self, length):
         self._items = [None] * length
-        self._index = 0
+        self._first = 0
+        self._last = 0
 
     def __len__(self):
-        return self._index
+        return (len(self._items) - self._first + self._last) % len(self._items)
 
     def is_empty(self):
-        pass
+        return self._front == self._last
 
     def is_full(self):
-        pass
+        return len(self) == len(self._items) - 1
 
     def dequeue(self):
 
         if self.is_empty():
             raise IndexError("Cannot dequeue from an empty queue.")
 
+        item = self._items[self._first]
+        self._items[self._first] = None
+        self._first = (self._first + 1) % len(self._items)
+        return item
+
     def enqueue(self, item):
 
         if self.is_full():
             raise IndexError("Cannot enqueue into a full queue.")
 
-        pass
+        self._items[self._last] = item
+        self._last = (self._last + 1) % len(self._items)
