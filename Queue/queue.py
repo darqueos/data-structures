2474254249d1257923b49baa601d37e14c4d66e5
Eduardo Quadros <quadros.ea@gmail.com>
2017-09-30 22:36:45 -0300

    Fix algorithm.

--- Queue/queue.py
+++ Queue/queue.py
@@ -3,18 +3,26 @@
 class Queue(object):
 
     def __init__(self, length):
-        self._items = [None] * length
-        self._first = 0
-        self._last = 0
+        self._items  = [None] * length
+        self._length = 0
+        self._first  = 0
+        self._last   = length - 1
 
     def __len__(self):
-        return (len(self._items) - self._first + self._last) % len(self._items)
+        return self._length
 
     def is_empty(self):
-        return self._front == self._last
+        return self._length == 0
 
     def is_full(self):
-        return len(self) == len(self._items) - 1
+        return self._length == len(self._items)
+
+    def peek(self):
+
+        if self.is_empty():
+            raise IndexError("Cannot peek at an empty queue.")
+
+        return self._items[self._first]
 
     def dequeue(self):
 
@@ -24,6 +32,7 @@ class Queue(object):
         item = self._items[self._first]
         self._items[self._first] = None
         self._first = (self._first + 1) % len(self._items)
+        self._length -= 1
         return item
 
     def enqueue(self, item):
@@ -31,5 +40,6 @@ class Queue(object):
         if self.is_full():
             raise IndexError("Cannot enqueue into a full queue.")
 
-        self._items[self._last] = item
         self._last = (self._last + 1) % len(self._items)
+        self._items[self._last] = item
+        self._length += 1
