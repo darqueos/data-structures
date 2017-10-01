cd155e1b81b750c77693c93236dc2de74aa14db0
Eduardo Quadros <quadros.ea@gmail.com>
2017-09-30 23:56:11 -0300

    Update algorithm.

--- Queue/Queue.swift
+++ Queue/Queue.swift
@@ -2,51 +2,63 @@
 
 internal final class Queue<T> {
 
-    private var items: Array<T?>
-    
-    internal init() {
-        self.items = []
-    }
-    
-    internal func isEmpty() -> Bool {
-        return self.items.count == 0
-    }
-    
-    internal func peek() throws -> T? {
-        guard !self.isEmpty() else {
-            throw QueueError.emptyQueue(reason: "Cannot peek at an empty queue!")
-        }
-        
-        return self.items.first as? T
-    }
-    
-    internal func pop() throws -> T? {
-        guard !self.isEmpty() else {
-            throw QueueError.emptyQueue(reason: "Cannot pop at an empty queue!")
-        }
-        
-        let item: T? = self.items.first  as? T
-        self.items.remove(at: 0)
-        return item
-    }
-    
-    internal func push(item: T?) throws -> Void {
-        self.items.append(item)
-    }
-    
-    internal func size() -> Int {
-        return self.items.count
-    }
+  internal var count: Int
 
-    self.items[self.index] = item
-    self.index += 1
+  private var items: Array<T?>
+  private var first: Int
+  private var last: Int
+
+  internal init(count: Int) {
+    self.items = [T?](repeating: nil, count: count)
+    self.count = 0
+    self.first = 0
+    self.last = length - 1
   }
 
-  internal func size() -> Int {
-    return self.index
+  internal func isEmpty() -> Bool {
+    return self.count == 0
   }
+
+  internal func isFull() -> Bool {
+    return self.count == self.items.count
+  }
+
+  internal func peek() throws -> T? {
+
+    guard !self.isEmpty() else {
+      throw QueueError.emptyQueue(reason: "Cannot peek at an empty queue!")
+    }
+
+    return self.items[self.first]
+  }
+
+  internal func dequeue() throws -> T? {
+
+    guard !self.isEmpty() else {
+      throw QueueError.emptyQueue(reason: "Cannot dequeue from an empty queue!")
+    }
+
+    let item: T? = self.items[self.first]
+    self.items[self.first] = nil
+    self.first = (self.first + 1) % self.items.count
+    self.count -= 1
+    return item
+  }
+
+  internal func enqueue(item: T?) throws -> Void {
+
+    guard !self.isFull() else {
+      throw QueueError.fullQueue(reason: "Cannot enqueue into a full queue!")
+    }
+
+    self.last = (self.last + 1) % self.items.count
+    self.items[self.last] = item
+    self.count += 1
+  }
+
 }
 
 internal enum QueueError: Error {
-    case emptyQueue(reason: String)
+  case emptyQueue(reason: String)
+  case fullQueue(reason: String)
 }
