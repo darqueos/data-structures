5b01c0d00a1551c01dd127f7c8cf3bbd98f4b30c
Eduardo Quadros <quadros.ea@gmail.com>
2017-10-03 02:46:21 -0300

    Refactor for class properties.

--- Stack/stack.py
+++ Stack/stack.py
@@ -1,41 +1,42 @@
-# 3.6.2
 
 class Stack(object):
 
     def __init__(self, length):
-        self._items = [None] * length
-        self._index = 0
+        self.__items = [None] * length
+        self.__index = 0
 
     def __len__(self):
-        return self._index
+        return self.__index
 
+    @property
     def is_empty(self):
-        return self._index == 0
+        return self.__index == 0
 
+    @property
     def is_full(self):
-        return self._index == len(self._items)
+        return self.__index == len(self.__items)
 
     def peek(self):
 
-        if self.is_empty():
+        if self.is_empty:
             raise IndexError("Cannot peek at an empty stack!")
 
-        return self._items[self._index - 1]
+        return self.__items[self.__index - 1]
 
     def pop(self):
 
-        if self.is_empty():
+        if self.is_empty:
             raise IndexError("Cannot pop from an empty stack!")
 
-        self._index -= 1
-        item = self._items[self._index]
-        self._items[self._index] = None
+        self.__index -= 1
+        item = self.__items[self.__index]
+        self.__items[self.__index] = None
         return item
 
     def push(self, item):
 
-        if self.is_full():
+        if self.is_full:
             raise IndexError("Cannot push into a full stack!")
 
-        self._items[self._index] = item
-        self._index += 1
+        self.__items[self.__index] = item
+        self.__index += 1
